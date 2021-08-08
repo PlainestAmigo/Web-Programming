f5480a7fdd866400eeed8d5f43caaca05cee4d25
Shrayash Dubey <shrayashdubey123@gmail.com>
2021-08-08 11:01:59 +0530

    create a web page that can print hello to the name of the peson in URL

--- Lecture3/hello/views.py
+++ Lecture3/hello/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "hello\index.html")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, brian")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request , "hello/greet.html", {
+        "name": name.capitalize()
+    })

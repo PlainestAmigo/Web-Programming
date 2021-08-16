1b2e8de5b67e68f654aeb482d32c24df09c0f275
Shrayash Dubey <shrayashdubey123@gmail.com>
2021-08-16 14:04:33 +0530

    Now saying hello to anyone via Html

--- DjangoGit/hello/views.py
+++ DjangoGit/hello/views.py
@@ -9,4 +9,6 @@ def shrayash(request):
     return HttpResponse("Hello, Brian!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "hello/greet.html",{
+        "name": name.capitalize()
+    })
